08e11fe96c9969cc78df29151781293d5ec8d24e
JavierJia <jianfeng.jia@gmail.com>
2013-12-10 15:07:30 -0800

    update treebaging missingdata

---
+++ project/treeBaggingOnFilledMissingData.m
@@ -0,0 +1,23 @@
+clear all;
+data = load ('data/filledk50.txt');
+train = data(1:50000,:);
+
+Y = train(:,2);
+X = train(:,3:size(data,2));
+[Xtr,Xte,Ytr,Yte] = splitData(X,Y,0.8);
+
+nc = 200;
+b = TreeBagger(nc,Xtr,Ytr,'oobpred','on');
+plot(oobError(b))
+xlabel('number of grown trees')
+ylabel('out-of-bag classification error')
+
+yhat = str2num(cell2mat(predict(b,Xte)));
+e = mean( double(Yte ~= yhat) );
+e
+
+test = data(50001:end,:);
+Xblind = test(:,3:end);
+Yblind = str2num(cell2mat(predict(b,Xblind)));
+submission = [50001:150000, Yblind];
+dlmwrite('results/treebagFilledMissdata.txt',submission,'delimiter',' ', 'precision',10); 
