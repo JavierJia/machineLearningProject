e8ed2351fa30e0a41d801edcb81b13c26570ddbe
JavierJia <jianfeng.jia@gmail.com>
2013-12-10 11:01:38 -0800

    add missing data

---
+++ project/missingData.m
@@ -0,0 +1,18 @@
+clear all;
+dataTrain = load ('data/phy_train.dat');
+dataTest = load('data/phy_test.dat');
+dataTest(:,2) = NaN;
+
+data = [dataTrain;dataTest];
+
+for i = [22:24,46:48]
+    data(find(data(:,i)==999),i) = NaN;
+end
+
+for i = [31,57]
+    data(find(data(:,i)==9999),i) = NaN;
+end
+
+%dlmwrite('data/missingData2NanAll.txt',data,'delimiter','\t');
+
+filled = knnimpute(data, 'Distance','euclidean');
